3429400e0631f41e342d27a79b0a9f19ce3af9fe
Jake <jaketbrown1119@gmail.com>
2018-12-01 21:55:11 -0500

    Merge branch 'master' of https://github.com/JosephAntony1/HoneyGuide
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- app/src/main/java/com/project/cmsc436/honeyguide/ShowPiece.java
+++ app/src/main/java/com/project/cmsc436/honeyguide/ShowPiece.java
@@ -2,24 +2,25 @@ package com.project.cmsc436.honeyguide;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.RatingBar;
-import android.widget.Toast;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.widget.ShareActionProvider;
+
+import java.util.ArrayList;
 
 
 public class ShowPiece extends AppCompatActivity {
 
 
-    TextView textView;
-    String item;
-    RatingBar ratingBar;
-    ImageView imageView;
-    Button share;
-    Button delete;
+    private TextView textView;
+    private String item;
+    private ImageView imageView;
+    private ArrayList<String> list = new ArrayList<String>();
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +28,8 @@ public class ShowPiece extends AppCompatActivity {
         setContentView(R.layout.activity_show_piece);
 
         Intent intent = getIntent();
+        list = intent.getStringArrayListExtra("pieces");
+
         item = intent.getStringExtra("piece");
 
         imageView = (ImageView) findViewById(R.id.imageView1);
@@ -35,36 +38,38 @@ public class ShowPiece extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.piece);
         textView.setText(item);
 
-        share = (Button) findViewById(R.id.shareButton);
-        share.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                shareIntent.setType("text/plain");
-                //TODO: need a name and a body from previous activity
-                String shareBody = item;
-                String shareSubject = item;
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-                shareIntent.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
-                shareIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
-
-                startActivity(Intent.createChooser(shareIntent, "Share using"));
-
-
-            }
-        });
-
-
-        ratingBar = findViewById(R.id.rating);
-        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                Toast.makeText(getApplicationContext(),"Your Selected Ratings  : " + String.valueOf(rating),Toast.LENGTH_LONG).show();
-            }
-        });
 
 
 
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.pieces_menu_fragment, menu);
+        return true;
+    }
+
+    public void onShareAction(MenuItem m) {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        String shareBody = item;
+        String shareSubject = item;
+
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+
+        startActivity(Intent.createChooser(shareIntent, "Share using"));
+    }
+
+    public void onGarbageAction(MenuItem m) {
+        list.remove(item);
+        Intent intent = new Intent(ShowPiece.this, ListPiecesView.class);
+        intent.putStringArrayListExtra("pieces", list);
+        startActivity(intent);
+    }
+
+
 }
