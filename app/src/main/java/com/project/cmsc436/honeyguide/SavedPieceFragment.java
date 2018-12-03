4595902ae1473e37a924b403b5346e59d3be976b
Maria Suplee <msuplee@umd.edu>
2018-12-03 17:16:39 -0500

    made new menu for saved pieces so you don't see the save button again

---
+++ app/src/main/java/com/project/cmsc436/honeyguide/SavedPieceFragment.java
@@ -0,0 +1,83 @@
+package com.project.cmsc436.honeyguide;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.Map;
+
+public class SavedPieceFragment extends Fragment {
+    private SharedPreferences sharedpreferences;
+
+    public static SavedPieceFragment newInstance() {
+        SavedPieceFragment fragment = new SavedPieceFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_piece, container, false);
+        MainActivity activity = (MainActivity) getActivity();
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+
+        //fetch the id received
+        String name = activity.getPiece();
+
+        //fetch content from local SharedPreference
+        sharedpreferences = activity.getSharedPreferences(name, Context.MODE_PRIVATE);
+        String date = sharedpreferences.getString("Date made",null);
+        String artist = sharedpreferences.getString("Artist",null);
+        String description1 = sharedpreferences.getString("Description1",null);
+        String description2 = sharedpreferences.getString("Description2",null);
+        String description3 = sharedpreferences.getString("Description3",null);
+        String url = sharedpreferences.getString("Image_URL",null);
+
+        //get references to views
+        ImageView art = (ImageView) view.findViewById(R.id.imageView);
+        TextView title = (TextView) view.findViewById(R.id.Title);
+        TextView date_artist = (TextView) view.findViewById(R.id.Date_Artist);
+        TextView p1 = (TextView) view.findViewById(R.id.Description1);
+        TextView p2 = (TextView) view.findViewById(R.id.Description2);
+        TextView p3 = (TextView) view.findViewById(R.id.Description3);
+
+        //update the view
+        //image downloading
+        String date_artist_str = date+", "+artist;
+        Picasso.with(activity.getApplicationContext()).load(url).into(art);
+        title.setText(name);
+        date_artist.setText(date_artist_str);
+        p1.setText(description1);
+        p2.setText(description2);
+        p3.setText(description3);
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.already_saved_piece, menu);
+    }
+}
